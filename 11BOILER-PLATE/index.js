af9dca4094769d744f4cda72c327e3835ec9c3bb
jsoyun <syusmm233@gmail.com>
2021-11-04 17:38:26 +0900

    토큰진행중

--- 11BOILER-PLATE/index.js
+++ 11BOILER-PLATE/index.js
@@ -1,21 +1,103 @@
+// const express = require("./node_modules/express");
 const express = require("express");
 const app = express();
 const port = 5000;
+// const bodyParser = require("./node_modules/body-parser");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json 분석해서 사용
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 //커밋하기 전에 이거 지우기!! 비번!!!
-const mongoose = require("moongoose");
+const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://<dbID>:<password>@boilerplatetest.erzhr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      //몽고디비 버전6이상은 이거없어도
-      //   useNewUrlParser: true,
-      //   useUnifiedTopology: true,
-      //   useCreateIndex: true,
-      //   useFindAndModify: false,
-    }
-  )
-  .then(() => console.log("MongoDB Connccccccccccvvvvvvvected.."))
+  .connect(config.mongoURI, {
+    //몽고디비 버전6이상은 이거없어도
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected.."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.post("/api/users/register", (req, res) => {
+  //회원가입할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터베이스에 넣어준다
+  //body-parser이용해서 req.body로 데이터 가져옴
+  const user = new User(req.body);
+  //몽고디비용 함수:정보들이 유저모델에 저장
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  //1.요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    //2.요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      //3.비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        //토큰을 저장한다. 어디에? 쿠키나 로컬스토리지,세션..등 우리는 쿠키에다 하겠다
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+// role 1 어드민   role 2 특정부서 어드민
+// role 0 -> 일반유저 role 0 이 아니면 관리자
+
+app.get("/api/users/auth", auth, (req, res) => {
+  //여기까지 미들웨어를 통과해왔다는 얘기는 Authentication이 True라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
